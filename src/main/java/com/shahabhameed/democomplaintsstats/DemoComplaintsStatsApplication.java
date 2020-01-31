b3c571a030c68ac38ba0181411a724368a518660
Muhammad Shahab <muhammad.shahab@globalrelay.net>
2020-01-31 15:13:04 -0800

    initial commit

---
+++ src/main/java/com/shahabhameed/democomplaintsstats/DemoComplaintsStatsApplication.java
@@ -0,0 +1,13 @@
+package com.shahabhameed.democomplaintsstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoComplaintsStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoComplaintsStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shahabhameed/democomplaintsstats/DemoComplaintsStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahabhameed.democomplaintsstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoComplaintsStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
